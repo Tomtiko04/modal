b979a302c72c78ad9075d73b273340293ba942de
Tomtiko04 <83877877+Tomtiko04@users.noreply.github.com>
2022-08-03 17:11:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+'use strict';
+// CREATE THE HIDDEN NUMBER
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+console.log(secretNumber);
+//THE CHECK CLICK EVENT
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
 
+    if (!guess) {
+        document.querySelector('.message').textContent = "⛔ No number!";
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = "😆 Guess matches!";
+        document.querySelector('body').style.backgroundColor = "green";
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "📉 Number is too high!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "You lost!";
+            document.querySelector('.score').textContent = 0;
+        }
+
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "📉 Number is too low!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "You lost!";
+            document.querySelector('.score').textContent = 0;
+        }
+
+    }
+});
+// AGAIN BUTTON
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+    document.querySelector('.message').textContent = "Start guessing...";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = "#222";
+    document.querySelector('.number').style.width = '15rem';
+    //document.querySelector('.number').textContent = secretNumber;
+})
